1d7d5f2a7797a1e812e53afd40604e6088b88cd1
Anna <missjellx@gmail.com>
2022-01-26 10:55:45 +0300

    feat: add media for main page

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,24 +1,32 @@
-import { NavLink } from 'react-router-dom';
 import Nav from './components/Nav';
 import './style.css';
 
 const Header = () => (
   <div className="header">
     <div className="logo"></div>
-    <nav className="header-nav">
-      <ul className="header-nav">
-       <Nav />
-      </ul>
-    </nav>
-    <div className="header-btns">
-      <button className="header-btn">
-        <div className="btn-icon-sign-in"></div>
-        <span className="btn-text">Вход</span>
-      </button>
-      <button className="header-btn">
-        <div className="btn-icon-lang"></div>
-        <span className="btn-text">Язык</span>
-      </button>
+    <div
+      className="burger"
+      onClick={(e) => {
+        e.target.classList.toggle('close-burger');
+        e.currentTarget.nextElementSibling.classList.toggle('show');
+      }}
+    ></div>
+    <div className="header-menu-container">
+      <nav className="header-nav">
+        <ul className="header-nav">
+          <Nav />
+        </ul>
+      </nav>
+      <div className="header-btns">
+        <button className="header-btn">
+          <div className="btn-icon-sign-in"></div>
+          <span className="btn-text">Вход</span>
+        </button>
+        <button className="header-btn">
+          <div className="btn-icon-lang"></div>
+          <span className="btn-text">Язык</span>
+        </button>
+      </div>
     </div>
   </div>
 );
